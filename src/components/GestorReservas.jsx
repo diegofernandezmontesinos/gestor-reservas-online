f1452f7a8efb05e6a3f78e71e88f0a5639c83370
Diego <dfmontesinos@gmail.com>
2025-01-11 09:01:04 +0100

    continue exercise

--- src/components/GestorReservas.jsx
+++ src/components/GestorReservas.jsx
@@ -1,9 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 
 const GestorReservas = () => {
+  const [message, setMessage] = useState("");
+  const [inputNameValue, setInputNameValue] = useState("");
+  const [inputSelectValue, setInputSelectValue] = useState("");
+  const [isDisabled, setDisabled] = useState(false);
+
+  const searchAvailableTable = () => {
+    setMessage("Searching disponibility");
+    setDisabled(true);
+
+    setTimeout(() => {
+      if (parseInt(inputSelectValue, 10) <= 6) {
+        setMessage(`Reserved table for ${inputNameValue}`);
+      } else {
+        setMessage(`No table available for ${inputNameValue}`);
+      }
+      setDisabled(false);
+    }, 2000);
+  };
+
+  const validValue = inputNameValue && inputSelectValue;
   return (
     <>
       <h1>Gestor de Reservas Online</h1>
+      <input
+        type="text"
+        placeholder="Client Name"
+        value={inputNameValue}
+        onChange={(e) => setInputNameValue(e.target.value)}
+      />
+      <label>
+        Number of Guests
+        <select
+          name="Number of Guests"
+          onChange={(e) => setInputSelectValue(e.target.value)}
+        >
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select>
+      </label>
+      <button
+        onClick={searchAvailableTable}
+        disabled={isDisabled || !validValue}
+      >
+        Reserve Table
+      </button>
+      {message && <p>Searching disponibility</p>}
     </>
   );
 };
--- src/components/GestorReservas.test.jsx
+++ src/components/GestorReservas.test.jsx
@@ -1,9 +1,94 @@
-import { screen, render } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import GestorReservas from "./GestorReservas";
+
+const renderGestorReservas = () => {
+  render(<GestorReservas />);
+};
 
 describe("Component GestorRestaurante", () => {
   it("should render Gestor Restaurante", () => {
-    render(<GestorReservas />);
+    renderGestorReservas();
     expect(screen.getByText("Gestor de Reservas Online")).toBeInTheDocument();
+  });
+
+  it("should show an input with placeholder Client Name", () => {
+    renderGestorReservas();
+    const input = screen.getByPlaceholderText("Client Name");
+    expect(input).toBeInTheDocument();
+  });
+
+  it("should show a button with text Reserve Table", () => {
+    renderGestorReservas();
+    const button = screen.getByText(/Reserve Table/i);
+    expect(button).toBeInTheDocument();
+  });
+
+  it("should show a select field", () => {
+    renderGestorReservas();
+    const select = screen.getByLabelText(/Number of Guests/i);
+    expect(select).toBeInTheDocument();
+  });
+
+  it("should enable the button if all fields are not empty", () => {
+    renderGestorReservas();
+    const input = screen.getByPlaceholderText("Client Name");
+    const select = screen.getByLabelText(/Number of Guests/i);
+    const button = screen.getByText(/Reserve Table/i);
+
+    expect(button).toBeDisabled();
+
+    fireEvent.change(input, { target: { value: "Diego" } });
+    fireEvent.change(select, { target: { value: "3" } });
+
+    expect(button).toBeEnabled();
+  });
+
+  it('should show "Searching disponibility" after clicking the button', () => {
+    renderGestorReservas();
+    const input = screen.getByPlaceholderText("Client Name");
+    const select = screen.getByLabelText(/Number of Guests/i);
+    const button = screen.getByText(/Reserve Table/i);
+
+    fireEvent.change(input, { target: { value: "Diego" } });
+    fireEvent.change(select, { target: { value: "3" } });
+
+    fireEvent.click(button);
+    expect(screen.getByText("Searching disponibility")).toBeInTheDocument();
+  });
+
+  it("should show no availability for more than 6 guests", async () => {
+    renderGestorReservas();
+    const input = screen.getByPlaceholderText("Client Name");
+    const select = screen.getByLabelText("Number of Guests");
+    const button = screen.getByText(/Reserve Table/i);
+
+    fireEvent.change(input, { target: { value: "Diego" } });
+    fireEvent.change(select, { target: { value: "8" } });
+    fireEvent.click(button);
+
+    expect(screen.getByText("Searching disponibility")).toBeInTheDocument();
+
+    await waitFor(() =>
+      expect(
+        screen.getByText("No tables available for Diego")
+      ).toBeInTheDocument()
+    );
+  });
+
+  it("should show 'reserved table for {name}' for 6 guests or less", async () => {
+    renderGestorReservas();
+    const input = screen.getByPlaceholderText("Client Name");
+    const select = screen.getByLabelText("Number of Guests");
+    const button = screen.getByText(/Reserve Table/i);
+
+    fireEvent.change(input, { target: { value: "Diego" } });
+    fireEvent.change(select, { target: { value: "6" } });
+    fireEvent.click(button);
+
+    expect(screen.getByText("Searching disponibility")).toBeInTheDocument();
+
+    await waitFor(() =>
+      expect(screen.getByText("Reserved table for Diego")).toBeInTheDocument()
+    );
   });
 });
